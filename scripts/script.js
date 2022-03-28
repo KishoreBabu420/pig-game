86368f7f5510e3565597382759d82076013e400a
kishore <kishore.sai37@gmail.com>
2022-03-28 20:23:24 +0530

    game starter

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,32 @@
 'use strict';
 
 // Selecting elements
-const player0El = document.querySelector('.player--0');
-const player1El = document.querySelector('.player--1');
-const score0El = document.querySelector('#score--0');
+const player0El = document.getElementById('player--0');
+const player1El = document.getElementById('player--1');
+const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
 
-const diceEl = document.querySelector('.dice');
-const btnNew = document.querySelector('.btn--new');
-const btnRoll = document.querySelector('.btn--roll');
-const btnHold = document.querySelector('.btn--hold');
+const diceEl = document.getElementById('dice');
+const btnNew = document.getElementById('btn--new');
+const btnRoll = document.getElementById('btn--roll');
+const btnHold = document.getElementById('btn--hold');
 
 let scores, currentScore, activePlayer, playing;
+
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current0El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+};
+
+init();
